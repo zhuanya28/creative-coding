57377a45100b94f4f0e2070b891fb3510c81b1b8
zhuanya28 <azhukova1402@gmail.com>
2023-04-27 10:49:10 +0800

    party tyler

--- image-n-sound/index.js
+++ image-n-sound/index.js
@@ -1,7 +1,7 @@
 let img;
 
 function preload() {
-  img = loadImage("assets/20190917-Tyler-01.webp");
+  img = loadImage("assets/120-1207564_tyler-the-creator-earfquake.jpg");
 }
 
 function setup() {
@@ -10,6 +10,6 @@ function setup() {
 }
 
 function draw() {
-  tint(200, 200, 0);
-  image(img, 0, 0);
+  tint(frameCount % 150, frameCount % 50, frameCount % 255);
+  image(img, 0, 0, 1000, 650);
 }
